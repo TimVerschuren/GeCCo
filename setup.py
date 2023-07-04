3cd1723e0fed179904b9f0130710361c317feb2b
TimVerschuren <81747141+TimVerschuren@users.noreply.github.com>
2023-07-04 11:41:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
     python_requires=">=3.10",
     entry_points={
         "console_scripts": [
-            "gecco = src.cli:main",
+            "gecco = GeCCo_src.cli:main",
         ]
     }
 )
